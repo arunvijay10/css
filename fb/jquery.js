ba0a266b6106504ce0ff78bcb262e49c75e09924
Parrot Live user <user@localhost.localdomain>
2020-08-12 15:16:40 +0000

    under dev

---
+++ fb/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+        fname:{
+            required:true,
+            minlength:4
+        }
+    }
+
+    })
+      
+})
